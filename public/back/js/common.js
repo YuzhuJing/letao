c95c2c137f9c8cf143d643546f7fa89fd24383c0
YuzhuJing <459761013@qq.com>
2018-04-06 23:43:58 +0800

    4.6电商全端初步开始

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -13,3 +13,43 @@ $(document).ajaxStart(function(){
 
 
 });
+
+$(function(){
+
+//  二级分类
+$(".category").click(function(){
+  $(this).next().stop().slideToggle();
+});
+
+  //顶部菜单栏切换显示功能
+$(".icon_menu").click(function(){
+  $(".lt_aside").toggleClass("hidemenu");
+  $(".lt_main").toggleClass("hidemenu");
+  $(".lt_topbar").toggleClass("hidemenu");
+  //console.log("hhh");
+});
+
+
+  //模态框
+$(".icon_logout").click(function(){
+  $('#logoutModal').modal('show')
+});
+
+  $("#logoutBtn").click(function(){
+    //console.log("hhh");
+
+    $.ajax({
+      url:"/employee/employeeLogout",
+      type:"get",
+      dataType:'json',
+      success:function(info){
+        //console.log(info);
+       if(info.success){
+         location.href = "login.html";
+       }
+      }
+    })
+  });
+
+
+})
